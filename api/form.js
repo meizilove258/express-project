a07dc66158ca022d32b34685f0e6362a10bdaf0d
zhangjinmei <1075487618@qq.com>
2022-07-26 15:06:52 +0800

    fix: allaal

--- api/form.js
+++ api/form.js
@@ -3,13 +3,13 @@ module.exports = (req, res) => {
     res.set('Access-Control-Allow-Methods', 'GET');
     res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.set('Access-Control-Allow-Credentials', 'true')
-    if (req.method === "OPTIONS") {
-        return res.status(204)
-    } else {
+    // if (req.method === "OPTIONS") {
+    //     return res.status(204)
+    // } else {
         return res.status(200).json({
             code: 1,
             msg: 'success',
             entry: '你拿到数据啦~'
         })
-    }
+    // }
 }
