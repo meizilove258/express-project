e97dd8f8dfc97fa63505213af43408499f27c5be
zhangjinmei <1075487618@qq.com>
2022-07-26 15:16:59 +0800

    feat: express

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
-module.exports = (req, res) => {
-    res.send('test')
-}
+const express = require('express')
+const app = express()
+const router = express.Router()
+const expressCors = require('express-cors')
+
+router.get('/api/form', (req, res) => {
+    res.status(200).json({
+        code: 1,
+        msg: 'success',
+        entry: '你拿到数据啦~'
+    })
+})
+
+app
+    .use(expressCors({
+        allowedOrigins: ['http://vd.bch.leju.com']
+    }))
+    .use(router)
+
+app.listen(() => {
+    console.log('server listen ......')
+})
